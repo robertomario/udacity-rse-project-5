d923625c5a5f6bbaff2833a051355aae2b65d36f
robertomario <roberto.buelvas@chrysalabs.com>
2021-09-24 23:07:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Robotics Software Engineer Nanodegree Program of Udacity
+
+Roberto Buelvas
+
+Project 5
---
+++ home_service/scripts/launch.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+xterm  -e  " gazebo " & sleep 5
+xterm  -e  " source /opt/ros/noetic/setup.bash; roscore" & sleep 5
+xterm  -e  " source /opt/ros/noetic/setup.bash; rosrun rviz rviz"
