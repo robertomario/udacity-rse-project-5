fbe4f9f0dc536987a2824d2fd203bc4d045c5b71
robertomario <roberto.buelvas@chrysalabs.com>
2021-09-29 01:10:09 -0400

    Fix test_navigation.sh

--- home_service/scripts/test_navigation.sh
+++ home_service/scripts/test_navigation.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 xterm  -e  " source /opt/ros/noetic/setup.bash; source ~/ros/roberto_ws/devel/setup.bash; roslaunch home_service world.launch " & sleep 5
-xterm  -e  " source /opt/ros/noetic/setup.bash; source ~/ros/roberto_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:='$(find home_service)/map/map.yaml' initial_pose_x:='8.0' initial_pose_y:='-5.0' initial_pose_a:='2.5' " & sleep 5
+xterm  -e  " source /opt/ros/noetic/setup.bash; source ~/ros/roberto_ws/devel/setup.bash; roslaunch home_service nav.launch " & sleep 5
 xterm  -e  " source /opt/ros/noetic/setup.bash; source ~/ros/roberto_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch " 
